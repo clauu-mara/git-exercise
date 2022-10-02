bdc20d55cd9be81de484e64a322f12f7612fb48e
claudia.maraciuc@evozon.com <claudia.maraciuc@evozon.onmicrosoft.com>
2022-10-02 22:39:17 +0300

    Add NUnitTest project

---
+++ NUnitTestClassLib/NUnitTestClassLib/NUnitTestClass.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTestClassLib
+{
+    [TestFixture]
+    public class NUnitTestClass
+    {
+
+        [Test]
+        public void FirstTest()
+        {
+            int x = 2, y = 3;   
+            Assert.AreEqual(x,y);
+
+        }
+    }
+}
